8b32b92ddf89a4a05ccda2a07c720ee1b6bbcf41
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2026-03-13 13:09:18 +0900

    add fieldoption

---
+++ src/week02/fieldoption.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("### 형식 지정자 적용 ###\n");
+    printf("12345678901234567890\n");
+    
+    printf("%10c\n", 'A');
+    printf("%10d\n", 128);
+    printf("%10lf\n", 3.1415926);
+    printf("%10.3lf\n", 3.1415926);
+    printf("%10.4s\n", "Love is");
+
+    return 0;
+}
