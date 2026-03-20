253a14a2a30c9044742f1e6e2144365bdb77d351
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2026-03-20 10:54:34 +0900

    add test, test.c

---
+++ src/week03/test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    char str[10];
+    int i;
+    float f;
+
+    printf("문자를 입력하세요: ");
+    scanf("%c", &ch);
+
+    printf("문자열을 입력하세요:");
+    scanf("%s", str);
+
+    printf("입력받은 문자: %c\n", ch);
+    printf("입력받은 문자열: %s\n", str);
+
+    printf("정수를 입력하세요: ");
+    scanf("%d", &i);
+
+    printf("실수를 입력하세요: ");
+    scanf("%f", &f);
+
+    printf("입력받은 정수: %d\n", i);
+    printf("입력받은 실수: %f\n", f);
+
+    return 0;
+}
