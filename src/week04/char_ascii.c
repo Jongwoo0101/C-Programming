56e3e55b91253d1a457d5e0011999d65c911ef99
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2026-03-27 11:40:53 +0900

    add char_ascii

---
+++ src/week04/char_ascii.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char grade = 'A';
+    char dollar = '$';
+    char digit = '7';
+
+    printf("Grade 값: %c\n", grade);
+    printf("Grade 아스키코드: %d\n", grade);
+
+    printf("Dollar Sign 값: %c\n", dollar);
+    printf("Dollar Sign 아스키코드: %d\n", dollar);
+    
+    printf("Digit 값: %c\n", digit);
+    printf("Digit 아스키코드: %d\n", digit);
+
+    return 0;
+}
