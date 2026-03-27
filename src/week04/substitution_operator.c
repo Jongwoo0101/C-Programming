215144cd04932c4bef9bb76a89f3f4ca4cdf33d0
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2026-03-27 12:09:34 +0900

    add substitution_operator

---
+++ src/week04/substitution_operator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    printf("a = %d\n", a);
+
+    int x, y, z;
+    printf("x = %d, y = %d, z = %d\n", x, y, z);
+
+    int num = 5, score = 3;
+    num += 3;
+    score += 2;
+    printf("num = %d\n", num);
+    printf("score = %d\n", score);
+
+    return 0;
+}
