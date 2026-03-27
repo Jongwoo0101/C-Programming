a0b8599ecbaa59cc360798f9b82a509c717b4dd7
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2026-03-27 11:42:07 +0900

    add float_const

---
+++ src/week04/float_const.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float fscore;
+    fscore = 589.73456F;
+
+    printf("fscore: %f\n", fscore);
+    printf("fscore: %.2f\n", fscore);
+    printf("fscore: %e\n", fscore);
+    printf("fscore: %E\n", fscore);
+    
+}
