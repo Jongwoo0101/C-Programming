5ffaff51898202b3cd3436381b42e16e67ff5133
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2026-03-27 11:40:23 +0900

    add ascii

---
+++ src/week04/ascii.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char alphabet = 'T';
+
+    printf("alphabet 변수의 값: %c\n", alphabet);
+    printf("알파벳 'T'에 해당하는 ASCII 코드 값: %d\n", alphabet);
+
+    return 0;
+}
