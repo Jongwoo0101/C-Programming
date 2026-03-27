dc987ba121b6c1dd621e3faf4d8aa6078ba9821f
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2026-03-27 12:09:03 +0900

    add arithmetic_operator

---
+++ src/week04/arithmetic_operator.c
@@ -0,0 +1,32 @@
+// 산술 연산자 사용하여 계산하기
+
+#include <stdio.h>
+
+int main() {
+    int a = 15, b = 4;
+
+    int sum = a + b;
+    int differnece = a - b;
+    int product = a * b;
+    int quotient = a / b;
+
+    printf("sum: %d\n", sum);
+    printf("difference: %d\n", differnece);
+    printf("product: %d\n", product);
+    printf("quotient: %d\n", quotient);
+
+    float x = 5.5, y = 2.3;
+
+    float floatSum = x + y;
+    float floatDifference = x - y;
+    float floatProduct = x * y;
+    float floatQuotient = x / y;
+
+    printf("floatSum = %f\n", floatSum);
+    printf("floatDifference = %f\n", floatDifference);
+    printf("floatProduct = %f\n", floatProduct);
+    printf("floatQuotient = %f\n", floatQuotient);
+
+    return 0;
+    
+}
