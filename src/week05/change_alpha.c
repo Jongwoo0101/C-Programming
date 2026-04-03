15c05421759a93cfdc986016422c1696f3abd6eb
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2026-04-03 11:48:41 +0900

    add change_alpha

---
+++ src/week05/change_alpha.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char ch;
+    printf("알파벳을 입력하세요: ");
+    scanf("%c", &ch);
+
+    if (ch >= 'a' && ch <='z') {
+        ch = (ch - 'a' + 3) % 26 + 'a';
+    }
+    else {
+        ch = (ch - 'A' + 3) % 26 + 'A';
+    }
+
+    printf("변경된 문자: %c\n", ch);
+
+    return 0;
+    
+}
