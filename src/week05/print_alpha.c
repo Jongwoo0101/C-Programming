c61cb3593a93c1eacda98ab80218e72967950aee
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2026-04-03 16:08:24 +0900

    add print_alpha

---
+++ src/week05/print_alpha.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int ku;
+    char cd = 'A';
+
+    printf("정수 숫자 입력: ");
+    scanf("%d", &ku);
+
+    if(ku != 100) {
+        cd++;
+        printf("cd: %c", cd);
+
+    }
+    else {
+        printf("cd: %c", cd);
+    }
+
+    return 0;
+}
