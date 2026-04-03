094ad8798925b0f0644f3e711b767fc76fbeace0
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2026-04-03 15:49:48 +0900

    add change_light

---
+++ src/week05/change_light.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+    int bulbState;
+
+    printf("전구의 현재 상태를 입력하세요 (-1 또는 1): ");
+    scanf("%d", &bulbState);
+
+    bulbState = ~bulbState + 1;
+
+    printf("변경된 전구의 상태: %d\n", bulbState);
+
+    return 0;
+}
