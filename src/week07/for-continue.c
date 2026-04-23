9eac0c324006ea990b2fdc6d68d52c43302d74cd
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2026-04-23 21:11:20 +0900

    for-continue 주석 추가

--- src/week07/for-continue.c
+++ src/week07/for-continue.c
@@ -3,9 +3,9 @@
 int main() {
     for (int i = 1; i <= 10; i ++ ) {
         if (i % 2 != 0) {
-            continue;
+            continue; // 참일 때 증감식으로 이동
         }
-        printf("%d\n", i);
+        printf("%d\n", i); // 홀수 이면 실행x
     }
 
     return 0;
